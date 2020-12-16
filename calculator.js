60d402d6c0cf1f53366393f9e951499b39c71398
William Tomé <william.tomenh@gmail.com>
2020-12-16 14:25:17 -0300

    mostrar valor convertido na tela.

--- calculator.js
+++ calculator.js
@@ -1,13 +1,17 @@
 $(function(){
-    $('#converter').click(() => {
-      const milha = 1609.34
-      const milhas = parseFloat($('#milhas').val()) * milha
-      const metros = parseFloat($('#metros').val()) * milhas
-      console.log(metros);
-    })
+  const conversao = $('#mostrar-conversao')
+  conversao.css('display', 'none')
 
-    $('#limpar').click(() => {
-      $('#milhas').val('')
-      $('#metros').val('')
-    })
+  $('#converter').click(() => {
+    const milha = 1609.34
+    const milhas = parseFloat($('#milhas').val()) * milha
+    const metros = parseFloat($('#metros').val()) * milhas
+    conversao.append(metros)
+    conversao.css('display', 'block')
+  })
+
+  $('#limpar').click(() => {
+    $('#milhas').val('')
+    $('#metros').val('')
+  })
 })
